6ae45de3f0ae6adf9951b477590af62bc7756712
irorere juliet <irorerejuliet@gmail.com>
2025-04-04 12:32:29 +0100

    added the stories to the navbar

---
+++ src/Components/Constant/StoriesData.js
@@ -0,0 +1,62 @@
+export const StoriesData = [
+  {
+    id: 1,
+    useranme: "adventure",
+    image: "/images/adventure.jpeg",
+  },
+  {
+    id: 2,
+    useranme: "car",
+    image: "/images/car.jpeg",
+  },
+  {
+    id: 3,
+    useranme: "frank",
+    image: "/images/frank.jpg",
+  },
+  {
+    id: 4,
+    useranme: "frendishipquote",
+    image: "/images/frienshipQuote.jpeg",
+  },
+  {
+    id: 5,
+    useranme: "gold",
+    image: "/images/gold.jpg",
+  },
+  {
+    id: 6,
+    useranme: "mountain",
+    image: "/images/mountain.jpeg",
+  },
+  {
+    id: 7,
+    useranme: "party",
+    image: "/images/party.jpeg",
+  },
+  {
+    id: 8,
+    useranme: "photographer",
+    image: "/images/photographer.jpeg",
+  },
+  {
+    id: 9,
+    useranme: "smilingGirl",
+    image: "/images/smilingGirl.jpeg",
+  },
+  {
+    id: 10,
+    useranme: "tolu",
+    image: "/images/tolu.jpg",
+  },
+  {
+    id: 11,
+    useranme: "vacation",
+    image: "/images/vacation.jpegg",
+  },
+  {
+    id: 12,
+    useranme: "lifstyle",
+    image: "/images/lifestyle.jpeg",
+  },
+];
--- src/Pages/Stories/Stories.jsx
+++ src/Pages/Stories/Stories.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { StoriesData } from '../../Components/Constant/StoriesData'
 
 const Stories = () => {
   return (
-    <div className='lg:max-w-[41vw] md:mx-w-[70vw]  sm:max-w-full max-w-full h-auto flex items-center gap-x-3.5 overflow-x-scroll'>
-      <Link to="/" className='flex items-center flex-col flex-shrink-0'>
-      <div className='w-16 h-16 rounded-full object-cover p-[2px] bg-green-600'>
-        <img src="/images/foodTray.jpeg" alt="" className='rounded-full w-full h-full object-cover p[2.5px] bg-black'/>
-      </div>
+    <div className="lg:max-w-[41vw] md:mx-w-[70vw]  sm:max-w-full max-w-full h-auto flex items-center gap-x-3.5 overflow-x-scroll">
+      <Link to="/" key={1} className="flex items-center flex-col flex-shrink-0">
+        <div className="w-16 h-16 rounded-full object-cover p-[2px] bg-green-600">
+          <img
+            src="/images/foodTray.jpeg"
+            alt="foodtray"
+            className="rounded-full w-full h-full object-cover p-[2.5px] bg-black"
+          />
+        </div>
       </Link>
+      {StoriesData.map((story) => (
+        <Link to="/" key={story.id} className="flex items-center flex-col flex-shrink-0">
+          <div className="w-16 h-16 rounded-full object-cover p-[2px] bg-green-600">
+            <img
+              src={story.image}
+              alt="foodtray"
+              className="rounded-full w-full h-full object-cover p-[2.5px] bg-black"
+            />
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Stories
